88e4c10b12ce61816298261145ca55898af2356c
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2023-10-20 10:30:46 +0100

    z

--- Desktop/test/e-learning/resources/views/master/layout.blade.php
+++ Desktop/test/e-learning/resources/views/master/layout.blade.php
@@ -8,6 +8,12 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.css" rel="stylesheet" />
     <style>
         body {
             background-image: url('{{ asset('storage/images/gradient_2.jpg') }}');
@@ -43,53 +49,42 @@
             <!-- Collapsible wrapper -->
             <div class="collapse navbar-collapse" id="navbarButtonsExample">
                 <!-- Left links -->
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav dropdown me-auto mb-2 mb-lg-0">
                     @auth
                         <li class="nav-item">
                             <a class="nav-link" href="#">Dashboard</a>
                         </li>
+
                     @endauth
                 </ul>
                 <!-- Left links -->
 
                 <div class="d-flex align-items-center">
                     @auth
-                        <ul class="navbar-nav">
-                          <!-- Avatar -->
-                          <li class="nav-item dropdown">
-                            <a
-                              class="nav-link dropdown-toggle d-flex align-items-center"
-                              href="#"
-                              id="navbarDropdownMenuLink"
-                              role="button"
-                              data-mdb-toggle="dropdown"
-                              aria-expanded="false"
-                            >
-                              <img
-                                src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img (31).webp"
-                                class="rounded-circle"
-                                height="22"
-                                alt="Portrait of a Woman"
-                                loading="lazy"
-                              />
+                        <div class="dropdown">
+                            <a class="dropdown-toggle d-flex align-items-center hidden-arrow" href="#"
+                                id="navbarDropdownMenuAvatar" role="button" data-mdb-toggle="dropdown"
+                                aria-expanded="false">
+                                <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle"
+                                    height="25" alt="Black and White Portrait of a Man" loading="lazy" />
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                              <li>
-                                <a class="dropdown-item" href="#">My profile</a>
-                              </li>
-                              <li>
-                                <a class="dropdown-item" href="#">Settings</a>
-                              </li>
-                              <li>
-                                <a class="dropdown-item" href="#">Logout</a>
-                              </li>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                                <li>
+                                    <a class="dropdown-item" href="#">My profile</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Settings</a>
+                                </li>
+                                <li>
+
+                                    <form method="get" action="{{ route('logout') }}">
+                                        @csrf
+                                        <button type="submit" class="dropdown-item">Logout</button>
+                                    </form>
+
+                                </li>
                             </ul>
-                          </li>
-                        </ul>
-                        <form method="get" action="{{ route('logout') }}">
-                            @csrf
-                            <button type="submit">Logout</button>
-                        </form>
+                        </div>
                     @endauth
                     @guest
 
@@ -117,5 +112,7 @@
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
 </script>
 <script src="https://kit.fontawesome.com/345971220e.js" crossorigin="anonymous"></script>
+<!-- MDB -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
 
 </html>
