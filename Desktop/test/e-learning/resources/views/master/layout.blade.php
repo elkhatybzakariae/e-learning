39fdc239ac69fa3faeefdc23400156ef849a262f
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2023-10-23 14:42:38 +0100

    z

--- Desktop/test/e-learning/resources/views/master/layout.blade.php
+++ Desktop/test/e-learning/resources/views/master/layout.blade.php
@@ -51,7 +51,6 @@
             /* Show the <ul> on hover */
         }
     </style>
-
 </head>
 
 <body>
@@ -87,29 +86,9 @@
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton" id="catlist">
                             </ul>
                         </div>
-                        <div class="dropdown nav-item dropend ">
+                        {{-- <div class="dropdown nav-item dropend ">
                             <a class="nav-link  dropdown-toggle-split" href=""
                                 data-mdb-toggle="dropdown dropdown-toggle" aria-expanded="false">Categories</a>
-
-                            {{-- <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                @foreach ($categories as $cat)
-                                    <li>
-                                        <div class="dropdown nav-item ">
-                                            <a class="dropdown-item "  href="#">{{ $cat->CatName }}</a>
-                                            <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                                                @foreach ($souscategories as $scat)
-                                                    <div class="">
-                                                        <li class="">
-                                                            <a class="dropdown-item"
-                                                                href="#">{{ $scat->SCatName }}</a>
-                                                        </li>
-                                                    </div>
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                    </li>
-                                @endforeach
-                            </ul> --}}
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                 @foreach ($categories as $cat)
                                     <li>
@@ -151,7 +130,7 @@
                                 @endforeach
                             </ul>
 
-                        </div>
+                        </div> --}}
                         @if (auth()->user()->roles->contains('role_name', 'client'))
                             <p>Welcome back ,{{ auth()->user()->FirstName }}</p>
                         @else
@@ -256,23 +235,7 @@
 <script src="https://kit.fontawesome.com/345971220e.js" crossorigin="anonymous"></script>
 <!-- MDB -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
-{{-- <script>
-    var catlist = document.getElementById('catlist');
-    var categorie = @json($categories);
-    var souscategorie = @json($souscategories);
-    var sujets = @json($sujets);
 
-    const newUl = document.createElement('ul');
-
-    data.forEach(function(category) {
-        const newLi = document.createElement('li');
-        newLi.textContent = category.CatName;
-        newUl.appendChild(newLi);
-    });
-
-    catlist.appendChild(newUl);
-    console.log(catlist);
-</script> --}}
 <script>
     var catlist = document.getElementById('catlist');
 
@@ -287,59 +250,44 @@
         const catdiv = document.createElement('div');
         const cata = document.createElement('a');
         cata.textContent = categorie.CatName;
-        catdiv.id = categorie.id_Cat + categorie.CatName;
+        catdiv.id = categorie.id_Cat;
         catdiv.classList.add('dropdown');
         catdiv.classList.add('nav-item');
         catdiv.classList.add('dropright');
         cata.classList.add('dropdown-item');
 
-        // catli.addEventListener('mouseover', function() {
-        //     catdiv.classList.remove('show'); // Show the sub ul
-        // });
+        catli.addEventListener('mouseover', function() {
+            var souscategorieList = document.createElement('ul');
+            souscategorieList.classList.add('dropdown-menu');
+            const souscat = souscategories.filter(function(e) {
+                return e.id_Cat == catdiv.id;
+            })
+            souscat.forEach(function(souscategorie) {
+                const souscatLi = document.createElement('li');
+                const souscatdiv = document.createElement('div');
+                const souscata = document.createElement('a');
+                souscata.textContent = souscategorie.SCatName;
 
-        // catli.addEventListener('mouseleave', function() {
-        //     catdiv.classList.add('show'); // Hide the sub ul when leaving
-        // });
+                souscatdiv.id = souscategorie.id_SCat;
+                souscatdiv.classList.add('dropdown');
+                souscatdiv.classList.add('nav-item');
+                souscatdiv.classList.add('dropright');
+                souscata.classList.add('dropdown-item');
 
-        var souscategorieList = document.createElement('ul'); // Rename the variable to avoid conflicts
-        souscategorieList.classList.add('dropdown-menu');
+                souscatdiv.appendChild(souscata);
+                souscatLi.appendChild(souscatdiv);
+                souscategorieList.appendChild(souscatLi);
 
-        souscategories.forEach(function(souscategorie) {
-            const souscatLi = document.createElement('li');
-            const souscatdiv = document.createElement('div');
-            const souscata = document.createElement('a');
-            souscata.textContent = souscategorie.SCatName; // Use the correct property
-
-            souscatdiv.id = souscategorie.id_SCat + souscategorie.SCatName;
-
-
-            // souscategorieList.style.display = 'none';
-            souscategorieList.addEventListener('mouseover', function() {
-                souscategorieList.style.display = 'block';
+                catdiv.appendChild(souscategorieList);
             });
-            souscategorieList.addEventListener('mouseleave', function() {
+
+            catli.addEventListener('mouseleave', function() {
                 souscategorieList.style.display = 'none';
             });
-            // if (souscategorie.id_Cat !== categorie.id_Cat) {
-            //     souscategorieList.classList.add('hide');
-            // }
-            souscatdiv.classList.add('dropdown');
-            souscatdiv.classList.add('nav-item');
-            souscatdiv.classList.add('dropright');
-            souscata.classList.add('dropdown-item');
-
-            souscatdiv.appendChild(souscata);
-            souscatLi.appendChild(souscatdiv);
-            souscategorieList.appendChild(souscatLi);
         });
 
-
-
-
         catdiv.appendChild(cata);
-        catdiv.appendChild(souscategorieList);
-        catli.appendChild(
-            catdiv);
+        catli.appendChild(catdiv);
         catlist.appendChild(catli);
     });
 
