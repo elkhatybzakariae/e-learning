4618359668b49d7c10e621f31df5be75b6a0ddcf
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2023-11-07 10:07:11 +0100

    z

--- Desktop/test/e-learning/app/Http/Controllers/WishListController.php
+++ Desktop/test/e-learning/app/Http/Controllers/WishListController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cour;
+use App\Models\WishList;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WishListController extends Controller
 {
-    //
+    public function index(){
+        $id=Auth::id();
+        $listid= WishList::where('id_U',$id)->get();
+        $ids = $listid->pluck('id_C')->all();
+        $listW = Cour::whereIn('id_C', $ids)->get();
+        return view('management.wishlist.index',compact('listW'));
+    }
 }
