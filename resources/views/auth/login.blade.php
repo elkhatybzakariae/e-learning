b2cc3510fa09bec6afe4a2602725f429f9ecfe28
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2023-12-22 12:18:01 +0100

    Z

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,17 +9,17 @@
     <title>Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('storage/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset('storage/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('storage/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('storage/css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <style>
         .bg-login-image {
-            background: url("{{ asset('storage/img/e-learning.jpg') }}");
+            background: url("{{ secure_asset('storage/img/e-learning.jpg') }}");
             background-position: end;
             background-size: cover;
         }
@@ -102,14 +102,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src=" {{ asset('storage/vendor/jquery/jquery.min.js') }}"></script>
-    <script src=" {{ asset('storage/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src=" {{ secure_asset('storage/vendor/jquery/jquery.min.js') }}"></script>
+    <script src=" {{ secure_asset('storage/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src=" {{ asset('storage/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src=" {{ secure_asset('storage/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src=" {{ asset('storage/js/sb-admin-2.min.js') }}"></script>
+    <script src=" {{ secure_asset('storage/js/sb-admin-2.min.js') }}"></script>
 
 </body>
 
