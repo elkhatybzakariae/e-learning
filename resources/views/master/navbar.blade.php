49c23b085233642749c29cf2ae9626a3634729a6
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2023-12-26 14:51:21 +0100

    Z

--- resources/views/master/navbar.blade.php
+++ resources/views/master/navbar.blade.php
@@ -172,7 +172,7 @@
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown no-arrow d-sm-none">
+        {{-- <li class="nav-item dropdown no-arrow d-sm-none">
             <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fa-solid fa-bars"></i>
@@ -194,7 +194,38 @@
                 </div>
             @endguest
             </div>
+        </li> --}}
+
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fa-solid fa-bars"></i>
+            </a>
+            <!-- Dropdown - User Information -->
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="{{ route('panier.index') }}" role="button">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </a>
+                <a href="{{ route('loginpage') }}" class="dropdown-item btn btn-outline-primary" style="border-radius: 20px;"
+                        role="button">Login</a>
+                    <a href="{{ route('registerpage') }}" class= " dropdown-item btn btn-primary" style="border-radius: 20px;"
+                        role="button">Register</a>
+                <a class="dropdown-item" href="#">
+                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Activity Log
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="{{ route('logout') }}" class="dropdown-item" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                </a>
+            </div>
         </li>
+
+
+
+
         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
         {{-- <li class="nav-item dropdown no-arrow d-sm-none">
             <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
