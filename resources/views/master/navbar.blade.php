90ac18e002c0f15fcdbbd734de63ebe9b7969c66
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2023-12-27 10:19:01 +0100

    Z

--- resources/views/master/navbar.blade.php
+++ resources/views/master/navbar.blade.php
@@ -422,36 +422,38 @@
 
 
         <div class="topbar-divider d-none d-sm-block"></div>
-        <li id="authM" class="nav-item dropdown no-arrow" >
+        
+                @guest
+                <li id="authM" class="nav-item dropdown no-arrow" >
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 <i class="fa-solid fa-bars"></i>
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="{{ route('panier.index') }}" role="button">
+                {{-- <a class="dropdown-item" href="{{ route('panier.index') }}" role="button">
                     <i class="fa-solid fa-cart-shopping"></i>
                 </a>
                 <a class="dropdown-item" href="#">
                     <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                     Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
-                @auth
+                </a> --}}
+                {{-- <div class="dropdown-divider"></div> --}}
+                {{-- @auth
                     <a href="{{ route('logout') }}" class="dropdown-item" data-target="#logoutModal">
                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                         Logout
                     </a>
-                @endauth
-                @guest
+                @endauth --}}
                     <a href="{{ route('loginpage') }}" class="dropdown-item btn btn-outline-primary"
                         style="border-radius: 20px;" role="button">Login</a>
                     <a href="{{ route('registerpage') }}" class= " dropdown-item btn btn-primary"
                         style="border-radius: 20px;" role="button">Register</a>
-                @endguest
+                
 
             </div>
         </li>
+        @endguest
         @auth
             <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
