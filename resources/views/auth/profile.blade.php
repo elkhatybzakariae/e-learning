1222c196f98cc88ed713f8352d4ec173fdc1fd4a
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2023-12-29 11:31:22 +0100

    Z

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -9,13 +9,15 @@
             <div id="content">
 
                 @include('master.navbar')
-                <div class="wrapper bg-white center-div"
-                    style="padding: 30px 50px;border: 1px solid #ddd;border-radius: 15px;margin: 10px auto;max-width: 600px;">
-                    <h4 class="pb-4 border-bottom">Personal Informations</h4>
+                
+                    
                     <form action="{{ route('update', $profile->id_U) }}" method="post">
                         @csrf
                         @method('put')
+                        <div class="wrapper bg-white center-div"
+                    style="padding: 30px 50px;border: 1px solid #ddd;border-radius: 15px;margin: 10px auto;max-width: 600px;">
                         <div class="py-2">
+                            <h4 class="pb-4 border-bottom">Personal Informations</h4>
                             <div class="row py-2">
                                 <div class="col-lg-12 col-sm-12 form-group row">
                                     <label for="FirstName" class="col-lg-4 col-sm-12 col-form-label">First Name</label>
@@ -55,43 +57,43 @@
                                     </div>
                                 </div> --}}
                         </div>
+                        </div>
+                        <div class="wrapper bg-white center-div"
+                    style="padding: 30px 50px;border: 1px solid #ddd;border-radius: 15px;margin: 10px auto;max-width: 600px;">
                         <div class="py-2">
-                            <fieldset>
-                                <legend>Profession Informations :</legend>
-                                {{-- <h4 class="pb-4 border-bottom">Profession Informations</h4> --}}
-                                <div class="row py-2">
-                                    <div class="col-lg-12 col-sm-12 form-group row">
-                                        <label for="FirstName" class="col-lg-4 col-sm-12 col-form-label">First Name</label>
-                                        <div class="col-lg-8 col-sm-12">
-                                            <input type="text" name="FirstName" class="form-control bg-light"
-                                                value="{{ $profile->FirstName }}">
-                                        </div>
+                            <h4 class="pb-4 border-bottom">Profession Informations</h4>
+                            <div class="row py-2">
+                                <div class="col-lg-12 col-sm-12 form-group row">
+                                    <label for="FirstName" class="col-lg-4 col-sm-12 col-form-label">First Name</label>
+                                    <div class="col-lg-8 col-sm-12">
+                                        <input type="text" name="FirstName" class="form-control bg-light"
+                                            value="{{ $profile->FirstName }}">
                                     </div>
-
-                                    <div class="col-lg-12 col-sm-12 form-group row">
-                                        <label for="lastname" class="col-lg-4 col-sm-12 col-form-label">Last Name</label>
-                                        <div class="col-lg-8 col-sm-12">
-                                            <input type="text" name="LastName" class="form-control bg-light"
-                                                value="{{ $profile->LastName }}">
-                                        </div>
-                                    </div>
-
-                                    <div class="col-lg-12 col-sm-12 form-group row">
-                                        <label for="phone" class="col-lg-4 col-sm-12 col-form-label">Phone Number</label>
-                                        <div class="col-lg-8 col-sm-12">
-                                            <input type="tel" name="Phone" class="form-control bg-light"
-                                                value="{{ $profile->Phone }}">
-                                        </div>
-                                    </div>
-
                                 </div>
-                                <div class="py-3 pb-4 border-bottom text-end">
-                                    <button type="submit" class="btn btn-primary mr-3">Save Changes</button>
+
+                                <div class="col-lg-12 col-sm-12 form-group row">
+                                    <label for="lastname" class="col-lg-4 col-sm-12 col-form-label">Last Name</label>
+                                    <div class="col-lg-8 col-sm-12">
+                                        <input type="text" name="LastName" class="form-control bg-light"
+                                            value="{{ $profile->LastName }}">
+                                    </div>
                                 </div>
-                            </fieldset>
+
+                                <div class="col-lg-12 col-sm-12 form-group row">
+                                    <label for="phone" class="col-lg-4 col-sm-12 col-form-label">Phone Number</label>
+                                    <div class="col-lg-8 col-sm-12">
+                                        <input type="tel" name="Phone" class="form-control bg-light"
+                                            value="{{ $profile->Phone }}">
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="py-3 pb-4 border-bottom text-end">
+                                <button type="submit" class="btn btn-primary mr-3">Save Changes</button>
+                            </div>
+                        </div>
                         </div>
                     </form>
-                </div>
             </div>
             
             @include('master.footer')
